94e22b12e646b9380fdd6a60144799469176aa9f
Mateus Fugita <mateushiwao@gmail.com>
2021-08-30 22:03:37 -0300

    feat: create global colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,13 @@
+export default {
+    heading: '#006B7F',
+    text: '#5C6660',
+  
+    background: '#FFFFFF',
+    background_blue: '#00A1BF',
+    shape: '#F0F0F0',
+    white: '#FFFFFF',
+    gray: '#CFCFCF',
+  
+    red: '#E83F5B',
+};
+  
