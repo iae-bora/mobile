f3b11a4cf0a430ee44c4cdc9cd97c0c0e874edc9
Mateus Fugita <mateushiwao@gmail.com>
2021-09-01 23:10:32 -0300

    feat: button style as props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native';
+import { StyleProp, ViewStyle } from 'react-native';
+import { TouchableOpacity, Text, StyleSheet, StyleSheetProperties, TouchableOpacityProps } from 'react-native';
 
 import colors from '../styles/colors';
 
 interface ButtonProps extends TouchableOpacityProps {
     title: string;
+    button_style?: StyleProp<ViewStyle>;
 }
 
-export function Button({ title, ...rest }: ButtonProps){
+export function Button({ title, button_style, ...rest }: ButtonProps){
     return (
         <TouchableOpacity 
-            style={styles.container}
+            style={[styles.container, button_style]}
             {...rest}
         >
             <Text style={styles.text}>
@@ -24,7 +26,7 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: colors.background_blue,
         height: 56,
-        width: 112,
+        width: 168,
         borderRadius: 16,
         justifyContent: 'center',
         alignItems: 'center'
@@ -32,5 +34,6 @@ const styles = StyleSheet.create({
     text: {
         fontSize: 16,
         color: colors.white,
+        textAlign: 'center'
     }
 })
