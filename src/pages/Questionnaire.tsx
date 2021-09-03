9b9a8edb5f5e83011c2d6b9553641443d6e240d7
Mateus Fugita <mateushiwao@gmail.com>
2021-09-03 20:26:21 -0300

    feat: add buttons list of answers

--- src/pages/Questionnaire.tsx
+++ src/pages/Questionnaire.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, View } from 'react-native';
+import { RadioButton, Text } from 'react-native-paper';
 import { useNavigation } from '@react-navigation/native';
 
 import colors from '../styles/colors';
@@ -8,33 +9,49 @@ import { Button } from '../components/Button';
 
 export function Questionnaire(){
     const navigation = useNavigation();
+    const [answerOne, setAnswerOne] = useState<string>('');
 
     async function handleSubmit(){
         navigation.navigate('Home');
     }
 
     return (
-        <View style={styles.container}>
-            <View style={styles.header}>
-                <Text style={styles.title}>Precisamos te conhecer melhor</Text>
+        <SafeAreaView style={styles.container}>
+            <ScrollView>
+                <View style={styles.header}>
+                    <Text style={styles.title}>Precisamos te conhecer melhor</Text>
 
-                <Text style={styles.subtitle}>
-                    Este simples questionário nos {'\n'}
-                    ajudará a conhecer melhor seu {'\n'}
-                    perfil e te recomendar melhores {'\n'}
-                    lugares
-                </Text>
-            </View>
+                    <Text style={styles.subtitle}>
+                        Este simples questionário nos {'\n'}
+                        ajudará a conhecer melhor seu {'\n'}
+                        perfil e te recomendar melhores {'\n'}
+                        lugares (só será necessário responder {'\n'}
+                        essa pesquisa uma vez 😊)
+                    </Text>
+                </View>
 
-            <View style={styles.questionContainer}>
-                <Text style={styles.question}>Qual gênero musical você mais gosta?</Text>
-            </View>
+                <View style={styles.questionContainer}>
+                    <Text style={styles.question}>Qual gênero musical você mais gosta?</Text>
 
-            <Button 
-                title='Confirmar'
-                onPress={handleSubmit}
-            />
-        </View>
+                    <RadioButton.Group onValueChange={answer => setAnswerOne(answer)} value={answerOne}>
+                        <RadioButton.Item style={styles.answer} label='Rock' value='Rock' />
+                        <RadioButton.Item style={styles.answer} label='Sertanejo' value='Sertanejo' />
+                        <RadioButton.Item style={styles.answer} label='Forró' value='Forró' />
+                        <RadioButton.Item style={styles.answer} label='Gospel' value='Gospel' />
+                        <RadioButton.Item style={styles.answer} label='Pop' value='Pop' />
+                        <RadioButton.Item style={styles.answer} label='Funk' value='Funk' />
+                        <RadioButton.Item style={styles.answer} label='Rap' value='Rap' />
+                    </RadioButton.Group>
+                </View>
+
+                <View style={styles.footer}>
+                    <Button 
+                        title='Confirmar'
+                        onPress={handleSubmit}
+                    />
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
@@ -42,12 +59,12 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         height: StatusBar.currentHeight,
-        alignItems: 'center',
-        paddingHorizontal: 20
+        paddingHorizontal: 20,
+        alignItems: 'center'
     },
     header: {
         alignItems: 'center',
-        paddingVertical: 40
+        paddingVertical: 20
     },
     title: {
         fontSize: 28,
@@ -69,8 +86,16 @@ const styles = StyleSheet.create({
     },
     question: {
         fontSize: 17,
-        paddingVertical: 20,
+        paddingVertical: 10,
         color: colors.heading,
         fontWeight: 'bold'
+    },
+    answer: {
+        height: 40,
+        paddingVertical: 0
+    },
+    footer: {
+        alignItems: 'center',
+        marginBottom: 50
     }
 })
